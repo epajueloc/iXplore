cc371146795112e6b5c0bda02dba2501cca60fb3
Enrique Pajuelo <epajuelo@nd.edu>
2016-07-11 18:42:36 +0200

    Initial commit

---
+++ MapViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MapViewController.swift
+//  iXplore
+//
+//  Created by Enrique Pajuelo on 7/11/16.
+//  Copyright © 2016 Enrique Pajuelo. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
